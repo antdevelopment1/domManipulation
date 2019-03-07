feb1fcea19f1422efa1e3468a7be6837b5f4c0aa
April Copes <antdevelopment1@gmail.com>
2019-03-06 21:30:49 -0500

    examples of different attributes we can target

--- scripts.js
+++ scripts.js
@@ -17,4 +17,30 @@ element = document.links[2].id;
 element = document.links[2].className;
 element = document.links[2].classList;
 
-console.log(element)
+// Forms
+element = document.forms;
+element = document.forms[0];
+element = document.forms[0].id;
+element = document.forms[0].method;
+element = document.forms[0].action;
+element = document.forms[0].classList;
+element = document.forms[0].className;
+element = document.forms[0].classList[0];
+
+// Inputs
+element = document.images;
+
+// Scripts
+element = document.scripts;
+element = document.scripts[0].getAttribute('src');
+
+// Looping the images
+let images = document.images;
+let imagesArray = Array.from(images);
+
+imagesArray.forEach((image) => {
+    console.log(image.src)
+})
+
+
+console.log(imagesArray)
